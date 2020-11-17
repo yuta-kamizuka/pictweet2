d3af36295b780c677b5c1e4e8de290cff83fb1b3
yuta-kamizuka <cybor_ggg@icloud.com>
2020-11-17 15:35:18 +0900

    投稿ページ実装

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -4,4 +4,10 @@ class TweetsController < ApplicationController
     # allメソッド。テーブルのすべてのデータを取得する
     # 今回は@tweetsというインスタンス変数に、tweetsテーブルのレコードをすべて代入した
   end
+
+  def new
+    @tweets = Tweet.new #クラスのインスタンス（レコード）を生成する
+  end
+
+
 end
