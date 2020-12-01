53eb2538fba61af535c35e449caa429771eeb31f
yuta-kamizuka <cybor_ggg@icloud.com>
2020-12-01 13:44:20 +0900

    ルーティング追加」

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # resources :tweets  この記述は7つのアクション全てを適用
   root to: 'tweets#index'#ツイートの一覧ページへ遷移するように設定
   # onlyオプションでアクションを指定したものだけを生成。不要なアクションを作らないことで余計なエラーを防ぐ
-  resources :tweets, only: [:index, :new, :create, :destroy]
+  resources :tweets, only: [:index, :new, :create, :destroy, :edit]
 
 end
